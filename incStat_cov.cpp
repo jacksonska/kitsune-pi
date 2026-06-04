36c01c7ed5cd7e0f1632673b5c85c88f536bc259
Jackson Skaaden <jassskaadennn@gmail.com>
2026-06-04 15:26:12 -0700

    incStat_cov.cpp: implement get_stats1,2,3,4

--- incStat_cov.cpp
+++ incStat_cov.cpp
@@ -49,12 +49,12 @@ double incStat_cov::processDecay(std::size_t time, int micro_inc_indx)
     return factor;
 }
 
-double cov()
+double incStat_cov::cov()
 {
     return this->cf3_ / this->w3_;
 }
 
-double pcc()
+double incStat_cov::pcc()
 {
     ss = this->incStats_[0].std() * this->incStats_[1].std();
     if (ss != 0)
@@ -62,4 +62,67 @@ double pcc()
         return this->cov() / ss;
     }
     return 0.0;
+}
+
+std::array<double,2> incStat_cov::get_stats1() // nrvo
+{
+    std::array<double,2> retval {this->cov(), this->pcc()};
+    return retval;
+}
+
+std::array<double,4> incStats_cov::get_stats2() // nrvo
+{
+    std::array<double,4> retval {
+        this->incStats_[0].radius(this->incStats_[1]),
+        this->incStats_[0].magnitude(this->incStats[1]),
+        this->cov(),
+        this->pcc()
+    };
+    return retval;
+}
+
+// NOTE: this presumes that incstats has values..
+std::array<double,8> incStats_cov::get_stats3()
+{
+    std::array<double,8>  retval {
+        this->incStats_[0].w_,
+        this->incStats_[0].mean(),
+        this->incStats_[0].std(),
+        this->incStats_[1].w_,
+        this->incStats_[1].mean(),
+        this->incStats_[1].std(),
+        this->cov(),
+        this->pcc()
+    };
+    return retval;
+}
+
+std::array<double,10> incStats_cov::get_stats4()
+{
+    std::array<double,10> retval {
+        this->incStats_[0].w_,
+        this->incStats_[0].mean(),
+        this->incStats_[0].std(),
+        this->incStats_[1].w_,
+        this->incStats_[1].mean(),
+        this->incStats_[1].std(),
+        this->incStats_[0].radius(this->incStats_[1]),
+        this->incStats_[0].magnitude(this->incStats_[1]),
+        this->cov(),
+        this->pcc()
+    };
+    return retval;
+}
+
+std::vector<std::string> getHeaders(std::size_t ver, bool suffix=true)
+{
+    std:vector<std::string> headers {};
+    headers.reserve(10);
+    std::string s0 {"0"};
+    std::string s1 {"1"};
+
+    if (suffix)
+    {
+        
+    }
 }
